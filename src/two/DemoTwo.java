40e17b13543ae9c08d386bd98645327b1131f044
ntkb4 <ntkjasur@gmail.com>
2021-09-30 21:06:45 -0400

    added condition of xyz user story

--- src/two/DemoTwo.java
+++ src/two/DemoTwo.java
@@ -4,6 +4,16 @@ public class DemoTwo {
 	public static void main(String[] args) {
 		
 		System.out.println("Demo Two..");
+		
+		System.out.println("Second commit...");
+		
+		boolean isSunday = true;
+		if (isSunday) {
+			System.out.println("We won..");
+			
+		} else {
+			System.out.println("yep..");
+		}
 	}
 
 }
